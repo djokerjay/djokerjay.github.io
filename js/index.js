e7bbcc44627783f0f34e91fa36f0f5173a5376cd
djokerjay <rgdev888@gmail.com>
2023-12-25 21:39:06 +0000

    Add Under Constructions

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const header = document.createElement("h1");
-  const container = document.getElementById("dvRoot");
-  header.innerHTML = "Hello from DJokerJay";
-
-  container.appendChild(header);
-});
+document.addEventListener("DOMContentLoaded", function () {});
