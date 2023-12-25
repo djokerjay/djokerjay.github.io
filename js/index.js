65b786991dea48a9baf46d268227dd014e20f14c
djokerjay <rgdev888@gmail.com>
2023-12-25 21:08:11 +0000

    Adding Javascript

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.createElement("h1");
+  const container = document.getElementById("dvRoot");
+  header.innerHTML = "Hello from DJokerJay";
+
+  container.appendChild(header);
+});
